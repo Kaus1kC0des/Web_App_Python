f91c9fa3af4ca53a22da36fa11f232bac9b94e3c
Kausik D <89984370+Kaus1kC0des@users.noreply.github.com>
2023-09-30 02:01:39 +0530

    Added a MySQL Database to store the posts have an error

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
+import pymysql as sql
 
+db = sql.connect(host='localhost',user='root',password='Govind@1950',database='FlaskApp')
 app = Flask(__name__)
-posts = {
-    0: {
-        'title': 'Hello, world',
-        'content': 'This is my first blog post!'
-    }
-}
-
+# posts = {
+#     0: {
+#         'title': 'Hello, world',
+#         'content': 'This is my first blog post!'
+#     }
+# }
+cursor = db.cursor()
+cursor.execute('SELECT * FROM posts')
+posts = {}
+values = cursor.fetchall()
+for i in range(len(values)):
+    posts[i] = {'title':values[i][1],'content':values[i][2]}
 
 
 
@@ -18,7 +25,9 @@ def home():
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
-    post = posts.get(post_id)
+    cursor = db.cursor()
+    cursor.execute(f'SELECT * FROM posts WHERE id = %s', (int(post_id)))
+    post = {'post_id':cursor.fetchone()[0],'title':cursor.fetchone()[1],'content':cursor.fetchone()[2]}
     if not post:
         return render_template('404.jinja2', message=f'A post with id {post_id} was not found.')
     return render_template('post.jinja2', post=post)
@@ -30,8 +39,10 @@ def create():
         title = request.form.get('title')
         content = request.form.get('content')
         post_id = len(posts)
-        posts[post_id] = {'id': post_id, 'title': title, 'content': content}
-
+        # posts[post_id] = {'id': post_id, 'title': title, 'content': content}
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO posts (title,content) VALUES (%s,%s)",(title,content))
+        db.commit()
         return redirect(url_for('post', post_id=post_id))
     return render_template('create.jinja2')
 
